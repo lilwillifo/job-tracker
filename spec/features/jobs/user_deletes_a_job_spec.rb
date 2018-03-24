ef7fc800f6bdab72359b8740a6f9426fbb1cbc92
lilwillifo <margaretwilliford4@gmail.com>
2018-03-24 17:51:46 -0600

    WIP deleting job test

--- spec/features/jobs/user_deletes_a_job_spec.rb
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -1,17 +1,17 @@
-# require 'rails_helper'
-#
-# describe "User deletes existing job" do
-#   scenario "a user can delete a job" do
-#     company = Company.create(name: "Turing")
-#     job = company.jobs.create!(title: "CoolJob", level_of_interest: 90, city: "Denver")
-#
-#     visit company_job_path(company, job)
-#
-#     # within(".company_#{company.id}") do
-#       click_link "Delete"
-#     # end
-#
-#     expect(page).to have_content("CoolJob was successfully deleted!")
-#     expect(current_path).to eq('/dashboard')
-#   end
-# end
+require 'rails_helper'
+
+describe "User deletes existing job" do
+  scenario "a user can delete a job" do
+    company = Company.create(name: "Turing")
+    job = company.jobs.create!(title: "CoolJob", level_of_interest: 90, city: "Denver")
+
+    visit company_job_path(company, job)
+
+    # within(".company_#{company.id}") do
+      click_link "Delete"
+    # end
+
+    expect(page).to have_content("CoolJob was successfully deleted!")
+    expect(current_path).to eq('/dashboard')
+  end
+end
