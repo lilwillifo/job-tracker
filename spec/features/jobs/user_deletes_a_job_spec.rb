7846142b1af0452891cf30ee64678b7d64688c7f
lilwillifo <margaretwilliford4@gmail.com>
2018-03-24 11:15:44 -0600

    testing for deleting job from job show page

---
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "User deletes existing job" do
+  scenario "a user can delete a job" do
+    company = Company.create(name: "Turing")
+    job = company.jobs.create!(title: "CoolJob", level_of_interest: 90, city: "Denver")
+
+    visit company_job_path(company, job)
+
+    # within(".company_#{company.id}") do
+      click_link "Delete"
+    # end
+
+    expect(page).to have_content("CoolJob was successfully deleted!")
+  end
+end
