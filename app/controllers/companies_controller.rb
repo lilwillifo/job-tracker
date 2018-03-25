be6e11ef0e0417d745b219a6a841da93e0976a99
VadLusk <AdLusk@gmail.com>
2018-03-25 11:04:18 -0600

    Add before action set company to company controller.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :destroy, :edit, :update]
+
   def index
     @companies = Company.all
   end
@@ -17,16 +19,11 @@ class CompaniesController < ApplicationController
     end
   end
 
-  def show
-    @company = Company.find(params[:id])
-  end
+  def show; end
 
-  def edit
-    @company = Company.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @company = Company.find(params[:id])
     @company.update(company_params)
     if @company.save
       flash[:success] = "#{@company.name} updated!"
@@ -37,8 +34,7 @@ class CompaniesController < ApplicationController
   end
 
   def destroy
-    company = Company.find(params[:id])
-    company.destroy
+    @company.destroy
 
     flash[:success] = "#{company.name} was successfully deleted!"
     redirect_to companies_path
@@ -49,5 +45,9 @@ class CompaniesController < ApplicationController
 
   def company_params
     params.require(:company).permit(:name, :city)
+  end
+
+  def set_company
+    @company = Company.find(params[:id])
   end
 end
--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe 'User sees one company' do
-  scenario 'a user sees a company' do
+  scenario 'a user sees a company and contacts' do
     company = Company.create!(name: 'ESPN')
     contact_1 = company.contacts.create!(name: 'Bob', role: 'Manager', email: 'Bob@espn.com')
     contact_2 = company.contacts.create!(name: 'Jan', role: 'CFO', email: 'Jan@espn.com')
 
     visit company_path(company)
 
-    expect(current_path).to eq('/companies/#{company.id}')
+    expect(current_path).to eq("/companies/#{company.id}")
     expect(page).to have_content(company.name)
     expect(page).to have_content(contact_1.name)
     expect(page).to have_content(contact_1.role)
@@ -17,7 +17,7 @@ describe 'User sees one company' do
     expect(page).to have_content(contact_2.role)
     expect(page).to have_content(contact_2.email)
   end
-  scenario 'they fill in a contact form' do
+  describe 'they fill in a contact form' do
     it 'displays the contact on the company show' do
       company = Company.create!(name: 'Dropbox')
 
