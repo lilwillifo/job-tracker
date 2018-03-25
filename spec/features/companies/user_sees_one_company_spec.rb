74c35106c9a78c5ab76797a3c60b64b54cda4e20
VadLusk <AdLusk@gmail.com>
2018-03-25 08:28:04 -0600

    Fix spec so it expects to have contact content, not job content.

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 describe "User sees one company" do
   scenario "a user sees a company" do
     company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
+    contact_1 = company.contacts.create!(name: 'Bob', role: 'Manager', email: 'Bob@espn.com')
+    contact_2 = company.contacts.create!(name: 'Jan', role: 'CFO', email: 'Jan@espn.com')
 
     visit company_path(company)
 
     expect(current_path).to eq("/companies/#{company.id}/jobs")
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
+    expect(page).to have_content(company.name)
+    expect(page).to have_content(contact_1.name)
+    expect(page).to have_content(contact_1.role)
+    expect(page).to have_content(contact_1.email)
+    expect(page).to have_content(contact_2.name)
+    expect(page).to have_content(contact_2.role)
+    expect(page).to have_content(contact_2.email)
   end
 end
