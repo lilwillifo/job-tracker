2c8d6d8a0a2db3a37b2a10b0124f9f43b0b6036b
lilwillifo <margaretwilliford4@gmail.com>
2018-03-25 17:06:48 -0600

    user sees all jobs associated with a category

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 describe 'User sees one category' do
   scenario 'a user sees a category and contacts' do
     category = Category.create!(title: 'stuff')
+    company = Company.create(name: 'company')
+    job_1 = company.jobs.create!(title: 'abc', description: '123', level_of_interest: 70, city: "Denver", category_id: category.id)
+    job_2 = company.jobs.create!(title: 'def', description: '456', level_of_interest: 5, city: "Denver", category_id: category.id)
 
     visit category_path(category)
 
     expect(current_path).to eq("/categories/#{category.id}")
     expect(page).to have_content(category.title)
+    expect(page).to have_content('abc')
+    expect(page).to have_content('def')
   end
 end
