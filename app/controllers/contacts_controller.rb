fd90ff88ce7382be2e0ba689c5b6e58cd451cf90
VadLusk <AdLusk@gmail.com>
2018-03-25 11:21:24 -0600

    Add contact_params.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,15 @@
 # app/controllers/contacts_controller.rb
 class ContactsController < ApplicationController
   def create
+    @contact = Contact.new(contact_params)
+    @contact.company_id = params[:company_id]
+    @contact.save
+    redirect_to company_path(@company)
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :role, :email, :company_id)
   end
 end
