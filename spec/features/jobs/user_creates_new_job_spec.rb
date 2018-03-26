9e4953d66f453617e427b61f2a05780e693c996d
lilwillifo <margaretwilliford4@gmail.com>
2018-03-25 19:57:55 -0600

    new job can only be rated 1 - 10

--- spec/features/jobs/user_creates_new_job_spec.rb
+++ spec/features/jobs/user_creates_new_job_spec.rb
@@ -8,7 +8,7 @@ describe "User creates a new job" do
 
     fill_in "job[title]", with: "Developer"
     fill_in "job[description]", with: "So fun!"
-    fill_in "job[level_of_interest]", with: 80
+    fill_in "job[level_of_interest]", with: "2"
     fill_in "job[city]", with: "Denver"
     select(category.title, from: 'Category')
 
@@ -17,7 +17,7 @@ describe "User creates a new job" do
     expect(current_path).to eq("/companies/#{company.id}/jobs/#{Job.last.id}")
     expect(page).to have_content("ESPN")
     expect(page).to have_content("Developer")
-    expect(page).to have_content("80")
+    expect(page).to have_content("2")
     expect(page).to have_content("Denver")
     expect(page).to have_content("Finance")
   end
