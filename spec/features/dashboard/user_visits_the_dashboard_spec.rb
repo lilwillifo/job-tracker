2c2c19e52a374451d7779fb747e0ca5331b8e1a4
VadLusk <AdLusk@gmail.com>
2018-03-26 19:38:36 -0600

    Add feature spec for same method.

--- spec/features/dashboard/user_visits_the_dashboard_spec.rb
+++ spec/features/dashboard/user_visits_the_dashboard_spec.rb
@@ -8,4 +8,35 @@ describe 'user visits the dashboard' do
     expect(page).to have_content('Jobs by Interest')
     expect(page).to have_content('Top Companies by Interest')
   end
+  context 'which displays correct stats' do
+    it 'like the count of jobs by level of interest' do
+      it '.count_by_level_of_interest' do
+        company = Company.create!(name: 'Google')
+        category = Category.create!(title: 'x')
+        company.jobs.create!(title: 'x', level_of_interest: 4, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 4, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 4, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 5, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 5, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 5, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 5, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 5, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 7, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 8, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 8, description: 'x', city: 'x', category: category)
+        company.jobs.create!(title: 'x', level_of_interest: 9, description: 'x', city: 'x', category: category)
+
+        expect(page).to have_content('* (0 jobs)')
+        expect(page).to have_content('** (0 jobs)')
+        expect(page).to have_content('*** (0 jobs)')
+        expect(page).to have_content('**** (3 jobs)')
+        expect(page).to have_content('***** (5 jobs)')
+        expect(page).to have_content('****** (0 jobs)')
+        expect(page).to have_content('******* (1 jobs)')
+        expect(page).to have_content('******** (2 jobs)')
+        expect(page).to have_content('********* (1 jobs)')
+        expect(page).to have_content('********** (0 jobs)')
+      end
+    end
+  end
 end
