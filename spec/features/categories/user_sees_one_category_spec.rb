ed28feb7e84b312d3f07964da69659ca548876da
lilwillifo <margaretwilliford4@gmail.com>
2018-03-26 19:43:55 -0600

    link to job from category show page

--- spec/features/categories/user_sees_one_category_spec.rb
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User sees one category' do
-  scenario 'a user sees a category and contacts' do
+  it 'sees a category and all its jobs' do
     category = Category.create!(title: 'stuff')
     company = Company.create(name: 'company')
     job_1 = company.jobs.create!(title: 'abc', description: '123', level_of_interest: 4, city: "Denver", category_id: category.id)
@@ -19,5 +19,16 @@ describe 'User sees one category' do
     expect(page).to have_content(job_2.company.name)
     expect(page).to have_content(job_2.level_of_interest)
     expect(page).to have_content(job_2.city)
+  end
+  it 'can link to a job' do
+    category = Category.create!(title: 'stuff')
+    company = Company.create(name: 'company')
+    job_1 = company.jobs.create!(title: 'abc', description: '123', level_of_interest: 4, city: "Denver", category_id: category.id)
+    job_2 = company.jobs.create!(title: 'def', description: '456', level_of_interest: 5, city: "Denver", category_id: category.id)
+
+    visit category_path(category)
+    click_on 'abc'
+
+    expect(current_path).to eq(company_job_path(company, job_1))
   end
 end
