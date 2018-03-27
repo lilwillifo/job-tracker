0aef2902810ec60bfa1ec1feb4e97b4be17075f6
VadLusk <AdLusk@gmail.com>
2018-03-26 18:26:01 -0600

    Add dashboard index to routes and change spec accordingly.

--- spec/features/dashboard/user_visits_the_dashboard_spec.rb
+++ spec/features/dashboard/user_visits_the_dashboard_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'user visits the dashboard' do
   it 'is totally possible' do
-    visit dashboard_path
+    visit dashboard_index_path
 
     expect(page).to have_content('Job Tracker')
     expect(page).to have_content('Jobs by Interest')
