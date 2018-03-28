8c8af8f621562cdd5e0d581453a8ec42449580f0
VadLusk <AdLusk@gmail.com>
2018-03-27 20:45:02 -0600

    Add tests for companies and categories indices links.

--- spec/features/user_navigates_with_nav_bar_spec.rb
+++ spec/features/user_navigates_with_nav_bar_spec.rb
@@ -11,4 +11,24 @@ describe 'user naviagates the site with navbar' do
 
     expect(current_path).to eq(jobs_path)
   end
+  scenario 'and can go to the companies index' do
+    company = Company.create!(name: "ESPN")
+    category = Category.create!(title: 'Finance')
+    job = company.jobs.create!(title: 'x', level_of_interest: 9, description: 'x', city: 'x', category: category)
+
+    visit companies_path
+    click_on 'Companies'
+
+    expect(current_path).to eq(companies_path)
+  end
+  scenario 'and can go to the categories index' do
+    company = Company.create!(name: "ESPN")
+    category = Category.create!(title: 'Finance')
+    job = company.jobs.create!(title: 'x', level_of_interest: 9, description: 'x', city: 'x', category: category)
+
+    visit companies_path
+    click_on 'Categories'
+
+    expect(current_path).to eq(categories_path)
+  end
 end
