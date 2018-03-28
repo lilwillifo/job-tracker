51a20a02172c4990f32c7c2b2a72cca525475c71
VadLusk <AdLusk@gmail.com>
2018-03-28 11:37:03 -0600

    Refactor top companies method.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,9 +1,9 @@
 class DashboardController < ApplicationController
   def index
     @top_companies = Company.top_companies_by_interest
-    @top_company_ids = @top_companies.map do |company|
-                          Company.find_by(name: company.name)
-                        end
+    # @top_company_ids = @top_companies.map do |company|
+    #                       Company.find_by(name: company.name)
+    #                     end
     @jobs = Job.all
     @job_locations = Job.group_by_location
   end
--- app/models/company.rb
+++ app/models/company.rb
@@ -4,9 +4,9 @@ class Company < ApplicationRecord
   has_many :contacts
 
   def self.top_companies_by_interest
-    select('companies.name, avg(jobs.level_of_interest) AS average_level')
+    select('companies.name, companies.id, avg(jobs.level_of_interest) AS average_level')
       .joins(:jobs)
-      .group(:name)
+      .group(:name, :id)
       .order('average_level DESC')
       .limit(3)
   end
