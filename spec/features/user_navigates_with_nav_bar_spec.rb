5a955fdd16b12a868fb37793835e38bf4d35f483
VadLusk <AdLusk@gmail.com>
2018-03-28 00:04:43 -0600

    Add lots of navbar flare, dropdown buttons, and passing test suite.

--- spec/features/user_navigates_with_nav_bar_spec.rb
+++ spec/features/user_navigates_with_nav_bar_spec.rb
@@ -6,8 +6,10 @@ describe 'user naviagates the site with navbar' do
     category = Category.create!(title: 'Finance')
     job = company.jobs.create!(title: 'x', level_of_interest: 9, description: 'x', city: 'x', category: category)
 
-    visit companies_path
-    click_on 'Jobs'
+    visit dashboard_index_path
+    within 'div.links' do
+      click_on 'Jobs'
+    end
 
     expect(current_path).to eq(jobs_path)
   end
@@ -21,6 +23,16 @@ describe 'user naviagates the site with navbar' do
 
     expect(current_path).to eq(companies_path)
   end
+  scenario 'and can go to the new company page' do
+    company = Company.create!(name: "ESPN")
+    category = Category.create!(title: 'Finance')
+    job = company.jobs.create!(title: 'x', level_of_interest: 9, description: 'x', city: 'x', category: category)
+
+    visit companies_path
+    click_on 'New Company'
+
+    expect(current_path).to eq(new_company_path)
+  end
   scenario 'and can go to the categories index' do
     company = Company.create!(name: "ESPN")
     category = Category.create!(title: 'Finance')
@@ -30,5 +42,25 @@ describe 'user naviagates the site with navbar' do
     click_on 'Categories'
 
     expect(current_path).to eq(categories_path)
+  end
+  scenario 'and can go to the new category page' do
+    company = Company.create!(name: "ESPN")
+    category = Category.create!(title: 'Finance')
+    job = company.jobs.create!(title: 'x', level_of_interest: 9, description: 'x', city: 'x', category: category)
+
+    visit companies_path
+    click_on 'New Category'
+
+    expect(current_path).to eq(new_category_path)
+  end
+  scenario 'and can go to the dashboard' do
+    company = Company.create!(name: "ESPN")
+    category = Category.create!(title: 'Finance')
+    job = company.jobs.create!(title: 'x', level_of_interest: 9, description: 'x', city: 'x', category: category)
+
+    visit companies_path
+    click_on 'Dashboard'
+
+    expect(current_path).to eq(dashboard_index_path)
   end
 end
