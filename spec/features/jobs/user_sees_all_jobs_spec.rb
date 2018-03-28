375adfd4af4287472909659fc69ae6144ca42e22
lilwillifo <margaretwilliford4@gmail.com>
2018-03-27 18:13:24 -0600

    rspec passing for sort

--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'User sees all jobs' do
-  scenario 'for a specific company' do
+  it 'for a specific company' do
     company = Company.create!(name: 'ESPN')
     category = Category.create!(title: 'Dev')
     company.jobs.create!(title: 'Developer', level_of_interest: 7, city: 'Denver', category_id: category.id)
@@ -13,7 +13,7 @@ describe 'User sees all jobs' do
     expect(page).to have_content('Developer')
     expect(page).to have_content('QA Analyst')
   end
-  scenario 'for all the companies' do
+  it 'for all the companies' do
     company_one = Company.create!(name: "ESPN")
     category = Category.create!(title: 'Dev')
     company_one.jobs.create!(title: "Developer", level_of_interest: 7, city: "Denver", category_id: category.id)
@@ -34,7 +34,7 @@ describe 'User sees all jobs' do
     expect(page).to have_content('Judge')
     expect(page).to have_content('Zoo Keeper')
   end
-  scenario 'for all the companies' do
+  it 'can seach for jobs by location' do
     company_one = Company.create!(name: "ESPN")
     category = Category.create!(title: 'Dev')
     company_one.jobs.create!(title: "Developer", level_of_interest: 7, city: "Denver", category_id: category.id)
@@ -50,5 +50,39 @@ describe 'User sees all jobs' do
     expect(page).to_not have_content('QA Analyst')
     expect(page).to have_content('Dev')
     expect(page).to have_content('Dentist')
+  end
+
+  it 'can sort the jobs page by location alphabetically' do
+    company_one = Company.create!(name: "ESPN")
+    category = Category.create!(title: 'Dev')
+    company_one.jobs.create!(title: "Developer", level_of_interest: 7, city: "Denver", category_id: category.id)
+    company_one.jobs.create!(title: "QA Analyst", level_of_interest: 9, city: "New York City", category_id: category.id)
+    company_two = Company.create!(name: "ESPN2")
+    company_two.jobs.create!(title: "Dev", level_of_interest: 2, city: "Denver", category_id: category.id)
+    company_two.jobs.create!(title: "Dentist", level_of_interest: 1, city: "Denver", category_id: category.id)
+
+    visit '/jobs'
+    click_on 'City'
+
+    within 'tr:nth(2)' do
+      expect(page).to have_content('Dev')
+    end
+  end
+
+  it 'can sort the jobs page by level of interest' do
+    company_one = Company.create!(name: "ESPN")
+    category = Category.create!(title: 'Dev')
+    company_one.jobs.create!(title: "Developer", level_of_interest: 7, city: "Denver", category_id: category.id)
+    company_one.jobs.create!(title: "QA Analyst", level_of_interest: 9, city: "New York City", category_id: category.id)
+    company_two = Company.create!(name: "ESPN2")
+    company_two.jobs.create!(title: "Dev", level_of_interest: 2, city: "Denver", category_id: category.id)
+    company_two.jobs.create!(title: "Dentist", level_of_interest: 1, city: "Denver", category_id: category.id)
+
+    visit '/jobs'
+    click_on 'Interest'
+
+    within 'tr:nth(2)' do
+      expect(page).to have_content('QA Analyst')
+    end
   end
 end
